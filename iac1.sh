ac66df706c2cd7747627ea60af21faf2355cf243
Ijovany Cecílio <132367847+ijovanycecilio@users.noreply.github.com>
2023-05-28 22:15:54 -0300

    Update iac1.sh

--- iac1.sh
+++ iac1.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo "Criando diretórios..."
 
+echo "Creating directories..."
 mkdir /publico
 mkdir /adm
 mkdir /ven
 mkdir /sec
 
-echo "Criando grupos de usuários..."
+echo "Creating user groups..."
 
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
-echo "Criando usuários..."
+echo "Creating users..."
 
 useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
 useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
@@ -27,7 +27,7 @@ useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
 
-echo "Especificando permissões dos diretórios...."
+echo "Specifying directory permissions..."
 
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
@@ -38,5 +38,5 @@ chmod 770 /ven
 chmod 770 /sec
 chmod 777 /publico
 
-echo "Fim....."
+echo "End..."
 
